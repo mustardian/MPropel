0e7595c0d35003985d107e4796112325e9195082
mustardian <68765272+mustardian@users.noreply.github.com>
2022-04-08 12:24:37 +0530

    noob mistake

--- javascript/calendar.js
+++ javascript/calendar.js
@@ -111,7 +111,7 @@ function formCalendar(examDates,obj){ //obj is required to handle importing the 
         // condition for if the day has an event
         
             for(let j = 0; j < examDates.length; j++){
-                if(Number(examDates[j][1]) === currentMonth+1 && Number(examDates[j][0]) === daysOfCurrentMonth[i] && Number(examDates[j][2]) === currentYear
+                if(Number(examDates[j][1]) === currentMonth+1 && Number(examDates[j][0]) === daysOfCurrentMonth[j] && Number(examDates[j][2]) === currentYear
                 && Number(examDates[j][0]) !== currentDay){
 
 
